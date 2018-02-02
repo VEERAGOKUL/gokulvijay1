04259c4440f61520e80d92b2dc3b903e061ae8b8
VEERAGOKUL <35995360+VEERAGOKUL@users.noreply.github.com>
2018-02-02 14:09:14 +0530

    VOWEL.C

---
+++ VOWEL.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int char;
+clrscr();
+printf("Enter the letter ,a);
+scanf("%d",&a);
+if(a==a a==e a==i a==o a==u)
+printf("VOWEL");
+else
+printf("CONSOLANT");
+return 0;
+}
