31da2af16019b2472d46ee763c84f1e0dffb74a8
VEERAGOKUL <35995360+VEERAGOKUL@users.noreply.github.com>
2018-02-02 14:26:36 +0530

    ALPHABET.C

---
+++ ALPHABET.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a;
+clrscr();
+printf("Enter the alphabet");
+scanf("%d",&a);
+if((a>='a')&&(a<=='z'))
+printf("It is alphabet");
+else
+printf("It is not alphabet");
+return 0;
+}
