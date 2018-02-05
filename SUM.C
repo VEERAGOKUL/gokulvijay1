15c03ff8439a6b2dc641cb1085279051204431d0
VEERAGOKUL <35995360+VEERAGOKUL@users.noreply.github.com>
2018-02-05 16:43:50 +0530

    Create SUM.C

---
+++ SUM.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int n,i,sum=0;
+ printf("Enter the positive value");
+ scanf("%d",&n);
+ for(i=0;i<=n;i++)
+ {
+  sum+=i;
+  }
+  printf("Sum=%d",sum);
+  getch();
+  return 0;
+  }
+  
+  
+ 
