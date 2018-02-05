606d40772cee48bc0dbb04b76d750309c5f50bf1
VEERAGOKUL <35995360+VEERAGOKUL@users.noreply.github.com>
2018-02-05 16:35:40 +0530

    Create HELLO.C

---
+++ HELLO.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include,conio.h>
+int main()
+{
+ int a,i;
+ printf("ENTER THE VALUE");
+ scanf("%d",&a);
+ for(i=0;i<=a;i++)
+ {
+ printf("Hey,hello",a);
+ }
+ getch();
+ return 0;
+ }
