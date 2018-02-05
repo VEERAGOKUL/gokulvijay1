f320e566d775cd9c78b62784bc3f8903a67976b9
VEERAGOKUL <35995360+VEERAGOKUL@users.noreply.github.com>
2018-02-05 16:04:44 +0530

    Create LARGE.C

---
+++ LARGE.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a,b;
+ clrscr();
+ printf("Enter the first number",a);
+ scanf(%d",&a);
+ printf("Enter the second number",&b);
+ scanf("%d",&b);
+ printf("The largest number is %d");
+ getch();
+ return 0;
+ }
