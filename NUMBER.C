11e240433aedd6f0b7eec1b5f8fdb1d9c22ab2ba
VEERAGOKUL <35995360+VEERAGOKUL@users.noreply.github.com>
2018-02-07 16:35:48 +0530

    Create NUMBER.C

---
+++ NUMBER.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.c>
+vvoid main()
+{
+ int n,k,result;
+ printf("\nEnter the n");
+ scanf("%d",&n);
+ printf("\nEnter the k");
+ scanf("%d",&k);
+ result=pow(n,k)
+ {
+  printf("The result is %d",result);
+  }
+   getch();
+   return 0;
+   }
