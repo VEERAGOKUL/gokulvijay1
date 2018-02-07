33662fce987c27e897dac16d523bad5ecb856521
VEERAGOKUL <35995360+VEERAGOKUL@users.noreply.github.com>
2018-02-07 15:58:24 +0530

    Create INTEGER07.02.2018.C

---
+++ INTEGER07.02.2018.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a;
+ clrscr();
+ int count=0;
+ printf("Enter the number");
+ scanf("%d",&a);
+ while(a=0)
+ {
+  a\=10
+  count++
+  }
+  printf("Number is digit %d",count);
+  getch();
+  return 0;
+  }
+ 
