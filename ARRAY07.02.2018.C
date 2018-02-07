96737a04be61f52066dddc4fc4ed445f16c247ff
VEERAGOKUL <35995360+VEERAGOKUL@users.noreply.github.com>
2018-02-07 16:19:29 +0530

    Create ARRAY07.02.2018.C

---
+++ ARRAY07.02.2018.C
@@ -0,0 +1,19 @@
+#include,stdio.h>
+#include<conio.h>
+int main()
+{
+ int n,i,k,sum=0,arr[100];
+ scanff("%d\n",&n);
+ scanf("%d\n",&k);
+ for(i=0;i<n;i++)
+ {
+  scanf("%d",arr[i]);
+  }
+  for(i=0;i<k;i++)
+  {
+   sum+=a[i]
+   }
+   scanf("%d",&sum);
+   getch();
+   return 0;
+   }
