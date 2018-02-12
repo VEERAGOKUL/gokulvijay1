71c7df4afe69dc1b80f92ff791a802a27d50416c
VEERAGOKUL <35995360+VEERAGOKUL@users.noreply.github.com>
2018-02-12 15:56:01 +0530

    Create TWO12.02.2018.C

---
+++ TWO12.02.2018.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a,i,k,count=0;
+ clrscr();
+ printf("Enter the prime number");
+ scanf("%d",&a);
+ if(i=0;i<a;i++)
+ {
+  printf("Enter the interval,i);
+  scanf("%d",&i);
+  printf("Enter the second interval,k);
+  scanf("5d",&k);
+  }
+  i=a-k
+  printf("Difference of two intervals are %d");
+  getch();
+  return 0;
+  }
