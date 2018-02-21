98653f07395ba8115ade2375f25eeef30b393991
VEERAGOKUL <35995360+VEERAGOKUL@users.noreply.github.com>
2018-02-21 16:35:51 +0530

    Create INDEX21.02.2018.C

---
+++ INDEX21.02.2018.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+ int a[10],i,m;
+ printf("n=");
+ scanf("%d",&m);
+for(i=0;i<m;i++)
+{
+ scanf("%d",&a[i]);
+}
+for(i=0;i<m;i++)
+{
+ printf("\n%d\t%d",a[i],i);
+ return 0;
+ }
