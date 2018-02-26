25c7d789ac9470bdbd3dfda104de56886df164d1
VEERAGOKUL <35995360+VEERAGOKUL@users.noreply.github.com>
2018-02-26 15:50:33 +0530

    Create HOUR26.02.2018.C

---
+++ HOUR26.02.2018.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+int a,b;
+printf("enter the time");
+scanf("%d",&a);
+b=a/60;
+c=a%60;
+printf("the time is %d %D ",b,c);
+getch();
+return 0;
+}
+ 
