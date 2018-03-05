2bab43ccd894824b02bdbe13430dcd594a1f660d
VEERAGOKUL <35995360+VEERAGOKUL@users.noreply.github.com>
2018-03-05 16:34:15 +0530

    Create HGROLWR05.03.2018.C

---
+++ HGROLWR05.03.2018.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+{
+	int n;
+	int max,min;
+	int arr[10],a[10];
+	int i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+{
+	scanf("%d",&arr[i]);
+}
+	for(i=0;i<n;i++)
+{
+	a[i]=arr[i];
+}
+	max=arr[0];
+	for(i=0;i<n;i++)
+  return 0;
+  }
